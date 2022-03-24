763f95cfa52693aa9fee9656dc87ce8688c8abb9
Gaurav Kamble <gauravkamble909@gmail.com>
2022-03-24 13:22:21 +0530

    spring security basic authentication and authorization implemented

---
+++ src/main/java/com/example/springsecuritydemo/SimpleController.java
@@ -0,0 +1,24 @@
+package com.example.springsecuritydemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+	
+	@RequestMapping("/")
+	public String returnHelloMessage() {
+		return "<h1>Hello</h1>";
+	}
+	
+	@RequestMapping("/user")
+	public String returnUser() {
+		return "<h1>Welcome...User</h1>";
+	}
+	
+	@RequestMapping("/admin")
+	public String returnAdmin() {
+		return "<h1>Welcome...Admin</h1>";
+	}
+
+}
